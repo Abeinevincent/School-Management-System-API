9d618488acbd9297ed7054b101da56a7ee5fe818
Abeinevincent <abeinevincent@gmail.com>
2023-05-09 16:25:49 +0300

    Updated Payments Route

--- routes/feespayments/payments.js
+++ routes/feespayments/payments.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-// INITIATE MOMO PAYMENT *******************************************************
+// Generate userId with a custom function
+const generateUniqueId = () => {
+  let dt = new Date().getTime();
+  let id = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    let r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return id;
+};
 
 router.post("/initiatepayment", async (req, res) => {
   // Define the headers to be sent in the POST request
@@ -24,38 +33,82 @@ router.post("/initiatepayment", async (req, res) => {
           headers,
         }
       );
-      return res.status(200).json(response.data.token);
+      const token = response.data.token;
+
+      //   REGISTER IPN URL
+      try {
+        const response = await axios.post(
+          `${process.env.PESAPAL_TOKEN_IPN_REGISTRAR}`,
+          {
+            URL: process.env.IPN_URL,
+            ipn_notification_type: process.env.IPN_NOTIICATION_TYPE,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const ipnId = response.data.ipn_id;
+
+        // return res.status(200).json(response.data);
+
+        // Initiate payment on users phone(momo)
+        const { amount, phone_number } = req.body;
+        try {
+          const dataFromUser = {
+            id: generateUniqueId(),
+            currency: "UGX",
+            amount: amount,
+            description:
+              "We have initiated a payment on the provided phone number, confirm with your pin to confirm.",
+            callback_url: process.env.CALLBACK_URL,
+            redirect_mode: "",
+            notification_id: ipnId,
+            branch: "Rwebiita PS",
+            billing_address: {
+              email_address: "rwebitaps@gmail.com",
+              phone_number,
+              country_code: "UG",
+              first_name: "John",
+              middle_name: "",
+              last_name: "Doe",
+              line_1: "Rwebita PS",
+              line_2: "",
+              city: "",
+              state: "",
+              postal_code: "",
+              zip_code: "",
+            },
+          };
+
+          const response = await axios.post(
+            `${process.env.PROMPT_INITITATION_URL}`,
+            dataFromUser,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          return res.status(200).json(response.data);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        }
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
     } catch (err) {
       console.log(err);
       return res.status(500).send(err);
     }
   };
 
-  // REGISTER IPN URL
-  const registerIPNUrl = async () => {
-    try {
-      const response = await axios.post(
-        `${process.env.PESAPAL_TOKEN_IPN_REGISTRAR}`,
-        {
-          URL: process.env.IPN_URL,
-          ipn_notification_type: process.env.IPN_NOTIICATION_TYPE,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${generateToken()}`,
-          },
-        }
-      );
-      return res.status(200).json(response.data);
-    } catch (error) {
-      console.log(error, error.message);
-      console.error("Failed to register IPN URL:", error.message);
-      console.error("Server response:", error.response.data);
-      return res.status(500).send(error);
-    }
-  };
-  registerIPNUrl();
+  generateToken();
 });
 
 module.exports = router;
